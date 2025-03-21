1ee4d0e9fccca46a222676a29cd0aadf75b5d9f9
pyToApkBuilder <aritrapal494@gmail.com>
2025-03-21 08:44:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,26 @@ import yfinance as yf
 def main(page: ft.Page):
     page.title = "Flet Stock App"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     def get_stock_data(e):
         symbol = stock_symbol.value.strip().upper()
         try:
-            data = yf.Ticker(symbol).history(period="1d")
-            result.value = f"Latest Price for {symbol}: {data['Close'].iloc[-1]}"
-            stock_symbol.value=""
+            data = yf.Ticker(symbol).history(period="1y")
+            if not data.empty:  # Check if data is not empty
+                result.value = f"Latest Price for {symbol}: {data.iloc[-1].Close}"
+            else:
+                result.value = f"No data available for {symbol}. Check the symbol or try later."
+            stock_symbol.value = ""
         except Exception as ex:
             result.value = f"Error: {ex}"
-            stock_symbol.value=""
+            stock_symbol.value = ""
         page.update()
 
     stock_symbol = ft.TextField(label="Enter Stock Symbol", width=200)
     result = ft.Text()
     fetch_button = ft.ElevatedButton(text="Fetch", on_click=get_stock_data)
 
-    page.add(ft.Column([stock_symbol, fetch_button, result],horizontal_alignment=ft.CrossAxisAlignment.CENTER,))
+    page.add(ft.Column([stock_symbol, fetch_button, result], horizontal_alignment=ft.CrossAxisAlignment.CENTER))
 
 ft.app(target=main)
