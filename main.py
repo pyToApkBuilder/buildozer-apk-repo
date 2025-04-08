0510daa24a12c23ef2e8fc6997555d7811dba86a
pyToApkBuilder <aritrapal494@gmail.com>
2025-04-08 06:21:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,144 +1,134 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.properties import NumericProperty, BooleanProperty
+from kivy.properties import ListProperty, NumericProperty, ObjectProperty, BooleanProperty
 from kivy.clock import Clock
 from kivy.vector import Vector
-from kivy.uix.relativelayout import RelativeLayout
-from kivy.graphics import Rectangle, Line, Ellipse, Color
-from kivy.core.image import Image as CoreImage
-import random
+from kivy.uix.label import Label
+from random import random
 
+class Ball(Widget):
+    velocity = ListProperty([0, 0])
+    friction = NumericProperty(0.90)
+    is_ball1 = BooleanProperty(False)
 
-class BallGame(RelativeLayout):
-    ball_x = NumericProperty(500)  
-    ball_y = NumericProperty(200)  
-    velocity = [0, 0]  
-    ball_size = 50 
+class Target(Widget):
+    pass
 
-    target_x = NumericProperty(random.randint(100,800))  
-    target_y = NumericProperty(random.randint(100,1800))  
-    target_size = 80  
-
-    ball_inside_target = BooleanProperty(False) 
-    
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.start_pos = (0, 0)
-        
-        with self.canvas.before:
-            self.bg = Rectangle(source="bg.png", pos=self.pos, size=self.size)
-        self.bind(size=self._update_bg, pos=self._update_bg)
-
-        with self.canvas:
-            Color(1,1,1,1)
-            self.target_outline = Ellipse(pos=(self.target_x - 5, self.target_y - 5), size=(self.target_size + 10, self.target_size + 10))
-
-            Color(0, 1, 0, 0.6)
-            self.target = Ellipse(pos=(self.target_x, self.target_y), size=(self.target_size, self.target_size))
-            
-            Color(1,0,1,0.8)
-            self.ball = Ellipse(pos=(self.ball_x,self.ball_y),size=(self.ball_size, self.ball_size))
-
-        self.score = 0
-        self.tries = 0
-
-        self.score_l = Label(text=f"Score: 0  Tries: 0",font_size = 40)
-
-        self.add_widget(self.score_l)
-
-        Clock.schedule_interval(self.update, 1 / 60.0)
-
-    def _update_bg(self, *args):
-        self.bg.pos = self.pos
-        self.bg.size = self.size
+class GameWidget(Widget):
+    ball1 = ObjectProperty(None)
+    ball2 = ObjectProperty(None)
+    target = ObjectProperty(None)
+    touch_start = ListProperty([0, 0])
+    current_touch = ListProperty([0, 0])
+    is_aiming = BooleanProperty(False)
+    score = NumericProperty(0)
+    tries = NumericProperty(0)
+    points = NumericProperty(100)
 
     def on_touch_down(self, touch):
-        self.start_pos = touch.pos
-        with self.canvas:
-            Color(0, 1, 1) 
-            touch.ud['line'] = Line(points=(touch.x, touch.y))
+        if self.is_aiming:  # Ignore new touches while aiming
+            return False
+        self.touch_start = touch.pos
+        self.current_touch = touch.pos
+        self.is_aiming = True
+        touch.grab(self)
+        return True
 
     def on_touch_move(self, touch):
-        if 'line' in touch.ud:
-            touch.ud['line'].points = [touch.ud['line'].points[0],
-                                       touch.ud['line'].points[1],
-                                       touch.x, touch.y]
+        if touch.grab_current is self:
+            self.current_touch = touch.pos
+        return super().on_touch_move(touch)
 
     def on_touch_up(self, touch):
-        if 'line' in touch.ud:
-            self.canvas.remove(touch.ud['line'])
-            
-        end_pos = touch.pos
-        swipe_vector = Vector(end_pos) - Vector(self.start_pos)
-        self.velocity = swipe_vector * 0.05 
-        
-        self.tries += 1
-        self.score_l.text = f"Score: {self.score}  Tries: {self.tries}"
+        if touch.grab_current is self:
+            delta = Vector(self.current_touch) - Vector(self.touch_start)
+            self.ball1.velocity = -delta * 0.3
+            self.tries += 1
+            self.points = int(100/(self.tries+1))
+            self.is_aiming = False
+            touch.ungrab(self)
+        return super().on_touch_up(touch)
 
     def update(self, dt):
+        for ball in [self.ball1, self.ball2]:
+            ball.velocity = Vector(ball.velocity) * ball.friction
+            ball.pos = Vector(ball.velocity) + ball.pos
+
+            # Wall collisions
+            if ball.x < 0:
+                ball.x = 0
+                ball.velocity[0] *= -1
+            if ball.right > self.width:
+                ball.right = self.width
+                ball.velocity[0] *= -1
+            if ball.y < 0:
+                ball.y = 0
+                ball.velocity[1] *= -1
+            if ball.top > self.height:
+                ball.top = self.height
+                ball.velocity[1] *= -1
+
+        # Ball collision
+        distance = Vector(self.ball1.center).distance(self.ball2.center)
+        if distance <= (self.ball1.width/2 + self.ball2.width/2):
+            normal = Vector(self.ball2.center) - Vector(self.ball1.center)
+            if normal.length() == 0:
+                return
+            normal = normal.normalize()
+
+            # Only resolve collision if balls are moving towards each other
+            relative_velocity = Vector(self.ball2.velocity) - Vector(self.ball1.velocity)
+            if normal.dot(relative_velocity) > 0:
+                return
+
+            # Mass ratio (assuming equal mass)
+            mass_ratio = 2.0
+            v1 = Vector(self.ball1.velocity)
+            v2 = Vector(self.ball2.velocity)
+
+            new_v1 = v1 - normal * normal.dot(v1 - v2) * mass_ratio
+            new_v2 = v2 - normal * normal.dot(v2 - v1) * mass_ratio
+
+            self.ball1.velocity = new_v1
+            self.ball2.velocity = new_v2
+
+            # Position correction
+            overlap = (self.ball1.width/2 + self.ball2.width/2) - distance
+            if overlap > 0:
+                correction = normal * (overlap * 1.1)
+                self.ball1.pos = Vector(self.ball1.pos) - correction
+                self.ball2.pos = Vector(self.ball2.pos) + correction
+
+        # Target check
+        if self.ball2.collide_widget(self.target):
+            self.score = self.score + int(100/self.tries)
+            self.tries = 0
+            self.points = 100
+            self.reset_game()
+
+    def reset_game(self):
+        self.ball1.center = (
+            self.width * 0.1 + random() * self.width * 0.8,
+            self.height * 0.1 + random() * self.height * 0.8
+        )
         
-        self.ball_x -= self.velocity[0]
-        self.ball_y -= self.velocity[1]
-
+        self.ball2.center = (
+            self.width * 0.1 + random() * self.width * 0.8,
+            self.height * 0.1 + random() * self.height * 0.8
+        )
         
-        screen_width, screen_height = self.width, self.height
+        self.ball1.velocity = [0, 0]
+        self.ball2.velocity = [0, 0]
+        self.target.center = (
+            self.width * 0.1 + random() * self.width * 0.8,
+            self.height * 0.1 + random() * self.height * 0.8
+        )
 
-        if self.ball_x <= 0:
-            self.ball_x = 0
-            self.velocity[0] = -self.velocity[0]
-        if self.ball_x + self.ball_size >= screen_width:
-            self.ball_x = screen_width - self.ball_size
-            self.velocity[0] = -self.velocity[0]
-        if self.ball_y <= 0:
-            self.ball_y = 0
-            self.velocity[1] = -self.velocity[1]
-        if self.ball_y + self.ball_size >= screen_height:
-            self.ball_y = screen_height - self.ball_size
-            self.velocity[1] = -self.velocity[1]
-
-        
-        self.velocity = [v * 0.98 for v in self.velocity]
-
-        
-        self.ball.pos = (self.ball_x, self.ball_y)
-       
-        self.check_target_collision()
-
-    def check_target_collision(self):
-       
-        ball_center_x = self.ball_x + self.ball_size / 2
-        ball_center_y = self.ball_y + self.ball_size / 2
-
-        target_center_x = self.target_x + self.target_size / 2
-        target_center_y = self.target_y + self.target_size / 2
-
-        
-        distance = ((ball_center_x - target_center_x) ** 2 + (ball_center_y - target_center_y) ** 2) ** 0.5
-
-        if distance < (self.target_size / 2) - (self.ball_size / 2):
-            self.ball_inside_target = True
-            self.success_message()
-        else:
-            self.ball_inside_target = False
-
-    def success_message(self):
-        self.score +=1
-        self.score_l.text = f"Score: {self.score}  Tries: {self.tries}"
-
-        self.ball_x, self.ball_y = (self.width / 2) - 25, (self.height / 2) - 100
-        self.velocity = [0, 0]
-
-        self.target_x = random.randint(50, self.width - 100)
-        self.target_y = random.randint(50, self.height - 100)
-
-        self.target.pos = (self.target_x, self.target_y)
-        self.target_outline.pos = (self.target_x - 5, self.target_y - 5)
-
-class BallGameApp(App):
+class PoolGameApp(App):
     def build(self):
-        return BallGame()
+        game = GameWidget()
+        Clock.schedule_interval(game.update, 1/60.)
+        return game
 
-if __name__ == "__main__":
-    BallGameApp().run()
+if __name__ == '__main__':
+    PoolGameApp().run()
